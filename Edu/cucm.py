e419a7dcd24ac3f3afe74b2a9461b6fee0c997dc
Andrei Teodoroiu <andrei.teodoroiu@ro.ibm.com@Andreis-MacBook-Pro.local>
2019-11-26 13:00:40 +0200

    cucm final

--- Edu/cucm.py
+++ Edu/cucm.py
@@ -1,13 +1,25 @@
 #!/usr/local/bin/python3
-
-numere_cumparate = range(+498741470000, +498741470011)
-numere_cumparate2 = range(+498741480000, +498741480010)
-numere_folosite = [+498741470001, +498741470002, +498741470010, +498741480007]
+import pandas as pd
+import numpy as np
 
 
+numere_cumparate = range(49874196471005, 49874196471010)
+numere_cumparate2 = range(498741480000, 498741480010)
 
 
 
+#aici importi numerele si pe faci int
+df = pd.read_excel("/Users/andrei.teodoroiu@ro.ibm.com/Desktop/script_DRX/test1111_11252019195201/phone.xls", sheet_name="phone")
+list1 = list(df['Directory Number 1'])
+
+numere_folosite = []
+for i in list1:
+    numere_folosite.append(int(i))
+
+
+
+
+#functia care verifica daca nr este in use sau nu
 def verificare_numar(a):
     for i in a:
         if i in numere_folosite:
@@ -25,4 +37,4 @@ print("Numere verificate in VIB2:",verificare_VIB2)
 lista_completa = [numere_cumparate, numere_cumparate2]
 for i in lista_completa:
     automatizare = verificare_numar(i)
-    print(automatizare", Numere folosite in: ")
+    print(automatizare)
